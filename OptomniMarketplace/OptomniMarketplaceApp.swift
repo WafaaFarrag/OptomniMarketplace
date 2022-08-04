ca04706e4bfc3958227368c1acebf3f40e84dfe1
wafaa.farrag <wafaa.farrag@expertapps.com.sa>
2022-08-04 22:33:07 +0200

    Initial Commit

---
+++ OptomniMarketplace/OptomniMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptomniMarketplaceApp.swift
+//  OptomniMarketplace
+//
+//  Created by Expert on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct OptomniMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
